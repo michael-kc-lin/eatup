a36f9772b8ccefc7d0b8f1f38de36c2841699423
Michael Lin <mike2311@gmail.com>
2015-12-15 16:11:55 +0000

    change database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151125152123) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "landingpages", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at", null: false
